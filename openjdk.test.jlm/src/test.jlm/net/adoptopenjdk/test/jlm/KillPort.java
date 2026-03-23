bccd260a7b18ef07e4f2f69967017d81756f8186
Kritika-Basak <141907438+Kritika-Basak@users.noreply.github.com>
2026-03-23 14:05:15 +0530

    Update KillPort.java

--- openjdk.test.jlm/src/test.jlm/net/adoptopenjdk/test/jlm/KillPort.java
+++ openjdk.test.jlm/src/test.jlm/net/adoptopenjdk/test/jlm/KillPort.java
@@ -3,26 +3,21 @@ import java.io.InputStreamReader;
 
 public class KillPort {
 
-    public static void main(String[] args) {
-        int port = 1234;
-
+    public static void killPort(int port) {
         try {
-            // Find PID using the port
-            Process findProcess = Runtime.getRuntime().exec("lsof -t -i:" + port);
+            Process process = Runtime.getRuntime().exec("lsof -t -i:" + port);
             BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(findProcess.getInputStream()));
+                    new InputStreamReader(process.getInputStream()));
 
             String pid = reader.readLine();
 
             if (pid != null) {
-                System.out.println("Port " + port + " is used by PID: " + pid);
+                System.out.println("Port " + port + " running with PID: " + pid);
 
-                // Kill the process
                 Runtime.getRuntime().exec("kill -9 " + pid);
-
-                System.out.println("Process killed. Port " + port + " closed.");
+                System.out.println("Port " + port + " killed successfully.");
             } else {
-                System.out.println("Port " + port + " is not in use.");
+                System.out.println("Port " + port + " is not running.");
             }
 
         } catch (Exception e) {

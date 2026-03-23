c638f290bcda90ba04e21a6e034f9d8440a70437
Kritika-Basak <141907438+Kritika-Basak@users.noreply.github.com>
2026-03-23 14:03:32 +0530

    Create KillPort.java

---
+++ openjdk.test.jlm/src/test.jlm/net/adoptopenjdk/test/jlm/KillPort.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class KillPort {
+
+    public static void main(String[] args) {
+        int port = 8080;
+
+        try {
+            // Find PID using the port
+            Process findProcess = Runtime.getRuntime().exec("lsof -t -i:" + port);
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(findProcess.getInputStream()));
+
+            String pid = reader.readLine();
+
+            if (pid != null) {
+                System.out.println("Port " + port + " is used by PID: " + pid);
+
+                // Kill the process
+                Runtime.getRuntime().exec("kill -9 " + pid);
+
+                System.out.println("Process killed. Port " + port + " closed.");
+            } else {
+                System.out.println("Port " + port + " is not in use.");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

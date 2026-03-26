399b9953e020a0faf8e1bf8e3bf72d8a6ab519b5
Kritika-Basak <141907438+Kritika-Basak@users.noreply.github.com>
2026-03-26 17:29:32 +0530

    Update KillPort.java

--- openjdk.test.jlm/src/test.jlm/net/adoptopenjdk/test/jlm/KillPort.java
+++ openjdk.test.jlm/src/test.jlm/net/adoptopenjdk/test/jlm/KillPort.java
@@ -7,34 +7,20 @@ public class KillPort {
 
     public static void killPort(int port) {
         try {
-            System.out.println("DEBUG: Checking port " + port);
+            System.out.println("DEBUG: Attempting to kill port " + port);
 
-            Process process = Runtime.getRuntime().exec("lsof -t -i:" + port);
-
-            BufferedReader stdInput = new BufferedReader(
-                    new InputStreamReader(process.getInputStream()));
+            Process process = Runtime.getRuntime().exec("fuser -k " + port + "/tcp");
+            process.waitFor();
 
             BufferedReader stdError = new BufferedReader(
                     new InputStreamReader(process.getErrorStream()));
 
-            String pid = stdInput.readLine();
-
-            if (pid != null) {
-                System.out.println("DEBUG: Found PID " + pid);
-
-                Process kill = Runtime.getRuntime().exec("kill -9 " + pid);
-                kill.waitFor();
-
-                System.out.println("DEBUG: Port " + port + " killed");
-            } else {
-                System.out.println("DEBUG: No process found on port " + port);
-            }
-
-            // Print errors (VERY IMPORTANT)
             String err;
             while ((err = stdError.readLine()) != null) {
-                System.out.println("ERROR: " + err);
+                System.out.println("fuser: " + err);
             }
+
+            System.out.println("DEBUG: Kill command executed for port " + port);
 
         } catch (Exception e) {
             e.printStackTrace();
